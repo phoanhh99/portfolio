710fc607352ec39a86f89f9a69294c9ae8aa4102
kurumeii <babybangunny@gmail.com>
2021-11-08 17:35:59 +0700

    First commit on branch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 
 const blurTransparent =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
 
 export default function Homepage() {
+  // const {theme, handleChange} = props
+
+  // if (typeof window !== 'undefined') {
+  //   if (localStorage.getItem('theme') === '') {
+  //     theme
+  //       ? localStorage.setItem('theme', 'halloween')
+  //       : localStorage.setItem('theme', 'fantasy')
+  //   } else {
+  //     const savedTheme = localStorage.getItem('theme')
+  //     savedTheme === 'halloween' ? handleChange(false) : handleChange(true)
+  //   }
+  // }
+
   return (
     <>
       <Head>
